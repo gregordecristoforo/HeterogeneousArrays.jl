762b5c9eee08ea16d5b59914b7a794eeb7b88f46
jullcifer <julls.mikhailova@gmail.com>
2026-02-27 09:47:45 +0100

    Fix deploydocs repo URL format

--- docs/make.jl
+++ docs/make.jl
@@ -14,10 +14,9 @@ makedocs(
 
 # Deploy documentation to GitHub Pages (only in CI/CD)
 if get(ENV, "CI", nothing) == "true"
-    github_url = get(ENV, "GITHUB_SERVER_URL", "https://github.com")
     github_repo = get(ENV, "GITHUB_REPOSITORY", "")
     deploydocs(
-        repo = "$github_url/$github_repo.git",
+        repo = "github.com/$github_repo.git",
         devbranch = "main",
         push_preview = false
     )

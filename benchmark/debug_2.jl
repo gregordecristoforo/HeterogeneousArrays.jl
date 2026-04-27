da3a8f4845490f3150d7e5822cd5a3b0e0b7c035
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2026-04-27 12:34:43 +0200

    Commit before writing a combinatorial benchmarking of array interface and unit handling system

--- benchmark/debug_2.jl
+++ benchmark/debug_2.jl
@@ -6,6 +6,7 @@ using Plots
 using BenchmarkTools
 using LinearAlgebra
 using ComponentArrays
+using HeterogeneousArrays
 
 function acceleration!(du::AbstractVector{<:Quantity},u::AbstractVector{<:Quantity}, p::AbstractVector{<:Quantity}, t)
     fd = -sign(u.v)*0.5*p.ρ*u.v^2*p.Cd*p.A
@@ -14,11 +15,11 @@ function acceleration!(du::AbstractVector{<:Quantity},u::AbstractVector{<:Quanti
     return du
 end
 
-u0 = ComponentVector(v=0.0u"m/s", h=100u"m")
-p  = ComponentVector(Cd=1.0u"", A=0.1u"m^2", ρ=1.0u"kg/m^3", m=50u"kg", g=9.81u"m/s^2")
+u0 = HeterogeneousVector(v=0.0u"m/s", h=100u"m")
+p  = HeterogeneousVector(Cd=1.0u"", A=0.1u"m^2", ρ=1.0u"kg/m^3", m=50u"kg", g=9.81u"m/s^2")
 
 tspan = (0.0u"s", 10.0u"s")
 prob = ODEProblem{true, OrdinaryDiffEq.SciMLBase.NoSpecialize}(acceleration!, u0, tspan, p,
- abstol=ComponentVector(v=1e-6u"m/s", h=1e-6u"m"), reltol=ComponentVector(v=1e-6u"m/s", h=1e-6u"m"))
+ abstol=HeterogeneousVector(v=1e-6u"m/s", h=1e-6u"m"), reltol=HeterogeneousVector(v=1e-6u"m/s", h=1e-6u"m"))
 sol = solve(prob, Tsit5())
 plt = plot(ustrip.(sol.t), [ustrip(u.v) for u in sol.u], label="Tsit5")

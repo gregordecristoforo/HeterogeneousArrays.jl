2640d5f05529cde57a37b96c5aa39484724570f6
Jakob Peder Pettersen <jakobpeder.pettersen@gmail.com>
2026-04-27 12:03:35 +0200

    Realized that the non-allocating interface is not working with FlexUnits when units are heterogeneous

--- benchmark/debug_2.jl
+++ benchmark/debug_2.jl
@@ -1,23 +1,24 @@
 using FlexUnits, .UnitRegistry
-# using OrdinaryDiffEq
-using DifferentialEquations
+using OrdinaryDiffEq
+# using DifferentialEquations
 using StaticArrays
 using Plots
 using BenchmarkTools
 using LinearAlgebra
 using ComponentArrays
 
-function acceleration(u::AbstractVector{<:Quantity}, p::AbstractVector{<:Quantity}, t)
+function acceleration!(du::AbstractVector{<:Quantity},u::AbstractVector{<:Quantity}, p::AbstractVector{<:Quantity}, t)
     fd = -sign(u.v)*0.5*p.ρ*u.v^2*p.Cd*p.A
-    dv = fd/p.m - p.g
-    dh = u.v
-    return ComponentVector(v=dv, h=dh)
+    du.v = fd/p.m - p.g
+    du.h = u.v
+    return du
 end
 
 u0 = ComponentVector(v=0.0u"m/s", h=100u"m")
 p  = ComponentVector(Cd=1.0u"", A=0.1u"m^2", ρ=1.0u"kg/m^3", m=50u"kg", g=9.81u"m/s^2")
 
 tspan = (0.0u"s", 10.0u"s")
-prob = ODEProblem{false, DifferentialEquations.SciMLBase.NoSpecialize}(acceleration, u0, tspan, p, abstol=[1e-6, 1e-6], reltol=[1e-6, 1e-6])
+prob = ODEProblem{true, OrdinaryDiffEq.SciMLBase.NoSpecialize}(acceleration!, u0, tspan, p,
+ abstol=ComponentVector(v=1e-6u"m/s", h=1e-6u"m"), reltol=ComponentVector(v=1e-6u"m/s", h=1e-6u"m"))
 sol = solve(prob, Tsit5())
 plt = plot(ustrip.(sol.t), [ustrip(u.v) for u in sol.u], label="Tsit5")

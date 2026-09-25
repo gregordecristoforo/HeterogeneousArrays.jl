5a973e48540c8eb2b5ac1d1a7c64f1cf3d4cd8b2
Gregor Decristoforo <gregor.decristoforo@gmail.com>
2026-09-25 10:51:57 +0200

    fix type instability in mapreduce

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,7 @@ using OffsetArrays
     include("test_broadcasting.jl")
     include("test_allocation.jl")
     include("test_nesting.jl")
+    include("test_reductions.jl")
     include("test_performance.jl")
     include("test_printing.jl")
 end
